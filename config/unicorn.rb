fc2e6a161352e21aa193f11c9ff42f7a572b4029
Pramod <pramodv_nyros@yahoo.com>
2014-11-20 10:14:25 +0530

    changed the code at unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,48 @@
 # config/unicorn.rb
+# Set your full path to application.
+app_dir = File.expand_path('../../', __FILE__)
 
+# Set unicorn options
+worker_processes 2
+preload_app true
+timeout 360
+
+# Fill path to your app
+working_directory app_dir
+
+# Set up socket location
+listen "#{app_dir}/tmp/sockets/unicorn.sock", :backlog => 64
+
+# Loging
+stderr_path "#{app_dir}/log/unicorn.stderr.log"
+stdout_path "#{app_dir}/log/unicorn.stdout.log"
+
+# Set master PID location
+pid "#{app_dir}/tmp/pids/unicorn.pid"
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
+
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{app_dir}/Gemfile"
+end
+
+
+=begin
 worker_processes 3
 timeout 30
 preload_app true
@@ -24,7 +67,7 @@ after_fork do |server, worker|
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
 end
-
+=end
 =begin
 
 worker_processes 4
